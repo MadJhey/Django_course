cdb553958b983a8e97ba196978cc4b12257317aa
Baranov Pavel <pavel-info@ya.ru>
2022-12-20 21:55:59 +0300

    Hello world, urls, view

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d+p0dy2%hhzwx*jfstfh#y*3nn()8b6$npams!@=2%m6whn=07
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.62', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
